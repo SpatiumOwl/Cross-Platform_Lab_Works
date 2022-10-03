51e092ee1c97c1e87bea5528654c0896cc4a6277
SpatiumOwl <54932621+SpatiumOwl@users.noreply.github.com>
2022-10-03 12:59:11 +0300

    Add files via upload

---
+++ Lab 01/Lab_01-Prog/Lab_01-Prog/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Lab_01_Prog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    public record Tile (int x, int y);
+    public static bool IsValid(Tile tile, int min, int max)
+    {
+        return tile.x >= min && tile.x <= max && tile.y >= min && tile.y <= max;
+    }
+    public static List<Tile> GenerateHorseMoves(Tile tile)
+    {
+        List<Tile> result = new List<Tile>();
+        if (IsValid(tile, 1, 10))
+
+    }
+}
